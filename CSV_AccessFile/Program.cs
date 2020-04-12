3ae1727b565ddd881efc14643742e32412169a3b
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-13 00:25:50 +0530

    Access the file

---
+++ CSV_AccessFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSV_AccessFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\The Daddy\source\repos\CSV_AccessFile\CSV_AccessFile\Text.txt";
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
