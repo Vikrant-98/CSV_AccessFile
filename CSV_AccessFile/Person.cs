3ffd8c36b3fbb9146247c62a00c363132a192946
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-13 00:45:49 +0530

    Store the File Data in string

---
+++ CSV_AccessFile/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_AccessFile
+{
+    public class Person
+    {
+        public string Name  { get; set; }
+
+        public string Age { get; set; }
+
+        public string Gmail { get; set; }
+
+    }
+}
--- CSV_AccessFile/Program.cs
+++ CSV_AccessFile/Program.cs
@@ -11,10 +11,23 @@ namespace CSV_AccessFile
         static void Main(string[] args)
         {
             string filepath = @"C:\Users\The Daddy\source\repos\CSV_AccessFile\CSV_AccessFile\Text.txt";
+            List<Person> people = new List<Person>();
             List<string> lines = File.ReadAllLines(filepath).ToList();
             foreach (string line in lines)
             {
-                Console.WriteLine(line);
+                String[] entries = line.Split(',');
+
+                Person newPerson = new Person();
+
+                newPerson.Name = entries[0];
+                newPerson.Age = entries[1];
+                newPerson.Gmail = entries[2];
+
+                people.Add(newPerson);
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.Name } { person.Age } : { person.Gmail }");
             }
         }
     }
