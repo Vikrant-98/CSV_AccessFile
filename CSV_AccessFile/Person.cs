ad7631e48807278ea8c8f3ab79d8b538933aaa8b
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-13 01:18:10 +0530

    Comments Added

--- CSV_AccessFile/Person.cs
+++ CSV_AccessFile/Person.cs
@@ -4,13 +4,16 @@ using System.Text;
 
 namespace CSV_AccessFile
 {
+    /// <summary>
+    /// Read the Name age and mail Id
+    /// </summary>
     public class Person
     {
         public string Name  { get; set; }
 
         public string Age { get; set; }
 
-        public string Gmail { get; set; }
+        public string Mail { get; set; }
 
     }
 }
--- CSV_AccessFile/Program.cs
+++ CSV_AccessFile/Program.cs
@@ -8,6 +8,10 @@ namespace CSV_AccessFile
 {
     class Program
     {
+        /// <summary>
+        /// Main method read the data from file
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string filepath = @"C:\Users\The Daddy\source\repos\CSV_AccessFile\CSV_AccessFile\Text.txt";
@@ -21,27 +25,27 @@ namespace CSV_AccessFile
 
                 newPerson.Name = entries[0];
                 newPerson.Age = entries[1];
-                newPerson.Gmail = entries[2];
+                newPerson.Mail = entries[2];
 
                 people.Add(newPerson);
             }
             foreach (var person in people)
             {
-                Console.WriteLine($"{ person.Name } { person.Age } : { person.Gmail }");
+                Console.WriteLine($"{ person.Name } { person.Age } : { person.Mail }");
             }
-            //new Data added in file
+            //New Data added in file
             Console.WriteLine("--------------------------------------------------------------------");
-            people.Add(new Person { Name = "Aditya" , Age = "24" , Gmail = "Adtiyas.kulkarni@gmail.com"});
+            people.Add(new Person { Name = "Aditya" , Age = "24" , Mail = "Adtiyas.kulkarni@gmail.com"});
             List<string> output = new List<string>();
             foreach (var person in people)
             {
-                output.Add($"{ person.Name },{ person.Age },{ person.Gmail }");
+                output.Add($"{ person.Name },{ person.Age },{ person.Mail }");
             }
             Console.WriteLine("Writing into the file");
             File.WriteAllLines(filepath, output);
             foreach (var person in people)
             {
-                Console.WriteLine($"{ person.Name } { person.Age } : { person.Gmail }");
+                Console.WriteLine($"{ person.Name } { person.Age } : { person.Mail }");
             }
         }
     }
